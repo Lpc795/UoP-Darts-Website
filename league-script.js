eccfa632540eea22dc9afa04492f89adb35888d0
louis <louispaul795@gmail.com>
2026-02-17 15:25:25 +0000

    Created league table with json and js files

---
+++ league-script.js
@@ -0,0 +1,22 @@
+async function loadLeagueTable() {
+  const res = await fetch("league-data.json");
+  const data = await res.json();
+
+  const tbody = document.querySelector("#league-body");
+
+  data.forEach(team => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${team.pos}</td>
+      <td>${team.team}</td>
+      <td>${team.p}</td>
+      <td>${team.w}</td>
+      <td>${team.l}</td>
+      <td>${team.gf}</td>
+      <td>${team.ga}</td>
+      <td>${team.gd}</td>
+    `;
+    tbody.appendChild(row);
+  });
+}
+loadLeagueTable();
